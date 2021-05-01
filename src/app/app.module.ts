a40ec2a8aff92bea8bb38674d3d8f5979c45cab4
fcesc-code <bpfran@gmail.com>
2021-05-01 10:16:45 +0200

    mockdata / added winedata.json and HttpClientModule in the app module to load the json data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { WineitemComponent } from './wineitem/wineitem.component';
 import { FormsModule } from '@angular/forms';
+import { WinelistComponent } from './winelist/winelist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    WineitemComponent
+    WineitemComponent,
+    WinelistComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     FormsModule
   ],
   providers: [],

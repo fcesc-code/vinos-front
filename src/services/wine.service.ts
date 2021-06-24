4eef18ab4b6d0de6ef1f7bb1224c0cbc5900e793
fcesc-code <bpfran@gmail.com>
2021-06-24 18:28:48 +0200

    feature / added error logging in wine service

--- src/services/wine.service.ts
+++ src/services/wine.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 import { IWineItem } from '../interfaces/items.interfaces';
 
 import { API_ENDPOINT } from '../api/conifg';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -18,43 +19,82 @@ export class WineService {
     }
 
   public getWines(): Observable<IWineItem[]> {
+    console.warn('wineService -> getWines called')
+    this.http.post(this.api, { something: 'yes' })
     return this.http.get<IWineItem[]>(this.api)
+      .pipe( retry(2),
+        catchError(this.handleError) 
+      )
   }
 
   public getWinesQuery( query: string ): Observable<IWineItem[]> {
+    console.warn('wineService -> getWinesQuery called')
     const formattedQuery = query.trim().toLowerCase();
     const queryParameters = { params: { q: formattedQuery } };
     return (formattedQuery && formattedQuery !== '')
       ? this.http.get<IWineItem[]>(`${this.api}`, queryParameters)
+        .pipe( retry(2),
+          catchError(this.handleError) 
+        )
       : this.getWines();
   }
 
   public getPaginatedWines( page: number ): Observable<IWineItem[]> {
+    console.warn('wineService -> getPaginatedWines called')
     return this.http.get<IWineItem[]>(`${this.api}/page/${page}`)
+      .pipe( retry(2),
+        catchError(this.handleError) 
+      )
   }
 
   public getWine( wineId: number ): Observable<IWineItem> {
+    console.warn('wineService -> getWine called')
     return this.http.get<IWineItem>(`${this.api}/${wineId}`)
+      .pipe( retry(2),
+        catchError(this.handleError) 
+      )
   }
 
   public createWine( wine: IWineItem ): Observable<IWineItem> {
+    console.warn('wineService -> createWine called with:', wine)
     return this.http.post<IWineItem>(this.api, wine)
+      .pipe( catchError(this.handleError) )
   }
 
   public deleteWine( wineId: number ): Observable<Object> {
+    console.warn('wineService -> deleteWine called')
     return this.http.delete<Object>(`${this.api}/${wineId}`)
+      .pipe( catchError(this.handleError) )
   }
 
   public getFilteredWines( parameter: string, value: string ): Observable<IWineItem[]> {
+    console.warn('wineService -> getFilteredWines called')
     return this.http.get<IWineItem[]>(`${this.api}/${parameter}/${value}`)
+      .pipe( retry(2),
+        catchError(this.handleError) 
+      )
   }
 
   public editWine( wine: IWineItem ): Observable<IWineItem> {
+    console.warn('wineService -> editWine called')
     return this.http.patch<IWineItem>(`${this.api}/edit`, wine)
+      .pipe( catchError(this.handleError) )
   }
 
-  public changeQuantity( wineId: number, newQuantity: number ): Observable<IWineItem> {
+  public changeQuantityInCart( wineId: number, newQuantity: number ): Observable<IWineItem> {
+    console.warn('wineService -> changeQuantityInCart called')
     return this.http.patch<IWineItem>(`${this.api}/${wineId}`, { quantityInCart: newQuantity })
+      .pipe( catchError(this.handleError) )
+  }
+
+  private handleError( error: HttpErrorResponse ){
+    ( error.status === 0 )
+      // A client-side or network error occurred.
+      ? console.error(`An error ocurred:`, error.error)
+      // The backend returned an unsuccessful response code. The response body main contain clues as to what went wrong.
+      : console.error(`Backend returned code ${error.status}, \nbody was: ${error.error}`)
+    // return an observable with a user-facing error message
+    return throwError(`Something bad happened; please try again later.`);
   }
 
 }
